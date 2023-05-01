44a01388b1b9908ab0fda17616492a3791d0f0ab
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-01 16:42:26 -0300

    feat: implementando 'class SimpleReport', req-02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,40 @@
+from datetime import date, datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products):
+        earliest_date = list(
+            map(
+                lambda fab_date: fab_date["data_de_fabricacao"],
+                products,
+            )
+        )
+
+        current_date = date.today()
+
+        valid_dates = [
+            valid_date["data_de_validade"]
+            for valid_date in products
+            if datetime.fromisoformat(valid_date["data_de_validade"]).date()
+            >= current_date
+        ]
+        closest_date = (
+            min(
+                valid_dates,
+                key=lambda x: abs(
+                    datetime.fromisoformat(x).date() - current_date
+                ),
+            )
+            if valid_dates
+            else None
+        )
+
+        companies = Counter(company["nome_da_empresa"] for company in products)
+
+        return (
+            f"Data de fabricação mais antiga: {min(earliest_date)}"
+            f"Data de validade mais próxima: {closest_date}"
+            f"Empresa com mais produtos: {companies.most_common(1)[0][0]}"
+        )
