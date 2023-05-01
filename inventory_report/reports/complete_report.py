1c57e5ff6020a75db87b69532a9a7e10ab17788f
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-01 17:31:00 -0300

    feat: implementando class CompleteReport,  req-03

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,17 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products):
+        simple_report = SimpleReport.generate(products)
+
+        companies = Counter(company["nome_da_empresa"] for company in products)
+
+        products_by_company = "Produtos estocados por empresa:\n"
+
+        for company, count in companies.items():
+            products_by_company += f"- {company}: {count}\n"
+
+        return f"{simple_report}\n{products_by_company}"
