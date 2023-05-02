a80750cec67f7e6d4c7de835866e61d385150cf2
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-01 22:37:02 -0300

    test: implementando test_relatorio_produto- req-08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,15 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = {
+        "id": 1,
+        "nome_do_produto": "carne",
+        "nome_da_empresa": "FreeBeef",
+        "data_de_fabricacao": "2023-01-20",
+        "data_de_validade": "2023-06-20",
+        "numero_de_serie": "33345456",
+        "instrucoes_de_armazenamento": "armazenar em freezer",
+    }
+    product = Product(**new_product)
+    assert product.__dict__ == new_product
